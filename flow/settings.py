6ae4d6c856cefed54b55cebcd102b2d92e159307
desarrollo-branar <102602710+desarrollo-branar@users.noreply.github.com>
2025-03-12 13:57:44 -0400

    Update settings.py

--- flow/settings.py
+++ flow/settings.py
@@ -101,9 +101,16 @@ WSGI_APPLICATION = "flow.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": os.environ.get("DB_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.environ.get("DB_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
+        "USER": os.environ.get("DB_USER", "user"),
+        "PASSWORD": os.environ.get("DB_PASSWORD", "password"),
+        "HOST": os.environ.get("DB_HOST", "localhost"),
+        "PORT": os.environ.get("DB_PORT", "5432"),
+        'OPTIONS': {
+            'sslmode': 'require',  # Habilita SSL
+        },
     }
 }
 
